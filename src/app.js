1abaa96b42bb73d0e186da1c2e426b5374899385
obendesmond <obendesmond2@gmail.com>
2022-05-05 10:05:09 +0100

    setup structure

--- src/app.js
+++ src/app.js
@@ -1,5 +1,28 @@
 import React from "react";
+import {
+  Footer,
+  Blog,
+  Features,
+  Possibility,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 }
---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./features.css";
+
+export default function Feature() {
+  return <div>Feature</div>;
+}
